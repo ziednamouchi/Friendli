9368a0483cd17b9cc0dd702f23af56f18a23fdf0
Zied Namouchi <zyed.namouchy@gmail.com>
2017-06-07 17:06:51 +0200

    Update files_menu.sh

--- scripts/files_menu.sh
+++ scripts/files_menu.sh
@@ -5,8 +5,23 @@ files_menu(){
 wallpaper
 echo " 1/ Add file"
 echo " 2/ Delete file"
-echo " 1/ Add directory"
-echo " 2/ Delete directory"
-echo " 3/ Back"
-echo " 4/ Exit"
+echo " 3/ Add directory"
+echo " 4/ Delete directory"
+echo " 5/ Back"
+echo " 6/ Exit"
 }
+
+files_menu
+read -p "FriendLi/files>" choice
+case $choice in
+ 1) sh /files_scripts/add_file.sh ;;
+ 2) sh /files_scripts/remove_file.sh ;;
+ 3) sh /files_scripts/add_dir.sh ;;
+ 4) sh /files_scripts/remove_dir.sh ;;
+ 5) sh ../menu_principal.sh ;;
+ 6) sh sc_exit.sh;;
+ *) echo "Bad choice !!!!"
+    clear; users_menu
+    read -p "FriendLi/files>" choice
+
+esac
