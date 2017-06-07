88427206c5bddcd4e2247f8f2dbaf403244c44d9
Zied Namouchi <zyed.namouchy@gmail.com>
2017-06-07 16:52:54 +0200

    Update remove.sh

--- scripts/files_scripts/remove.sh
+++ scripts/files_scripts/remove.sh
@@ -1,6 +1,13 @@
-read -p "Enter the file name" file
+choice="y"
+while $choice="y"
+do
+read -p "Enter the file name: " file
 if test -e ~/$file; then
 echo " [!] The file will be deleted from your home directory!"
 rm ~/$file
+read -p "Do you want to remove another file?[n/y]: " choice
 else
 echo " [-] The file doesn't exist!"
+read -p "Do you want to remove another file?[n/y]: " choice
+fi
+done
