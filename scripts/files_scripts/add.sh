8b645fc87bcca90f891ea488ac114f195dba024b
Zied Namouchi <zyed.namouchy@gmail.com>
2017-06-07 16:50:28 +0200

    Update add.sh

--- scripts/files_scripts/add.sh
+++ scripts/files_scripts/add.sh
@@ -1,7 +1,13 @@
+choice="y"
+while $choice="y"
+do
 read -p "Enter the name of the file" file
 if test -e $file; then
 echo "[!] The file you wan to create already exists !!"
+read -p "Do you want to choose another file? [n/y]" choice
 else
 echo "[+] The file will be created in your home directory!"
 touch ~/$file
+read -p "Do you want to choose another file? [n/y]" choice
 fi
+done
